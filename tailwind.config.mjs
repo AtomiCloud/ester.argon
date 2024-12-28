537798be757c2ebe8ce2a960f5e1bcc6876af813
kirinnee <kirinnee97@gmail.com>
2024-12-28 18:09:24 +0800

    feat: hero section complete

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -50,6 +50,42 @@ export default {
           4: 'hsl(var(--chart-4))',
           5: 'hsl(var(--chart-5))',
         },
+        'color-1': 'hsl(var(--color-1))',
+        'color-2': 'hsl(var(--color-2))',
+        'color-3': 'hsl(var(--color-3))',
+        'color-4': 'hsl(var(--color-4))',
+        'color-5': 'hsl(var(--color-5))',
+      },
+      animation: {
+        grid: 'grid 15s linear infinite',
+        rainbow: 'rainbow var(--speed, 2s) infinite linear',
+        pulse: 'pulse var(--duration) ease-out infinite',
+      },
+      keyframes: {
+        grid: {
+          '0%': {
+            transform: 'translateY(-50%)',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+        },
+        rainbow: {
+          '0%': {
+            'background-position': '0%',
+          },
+          '100%': {
+            'background-position': '200%',
+          },
+        },
+        pulse: {
+          '0%, 100%': {
+            boxShadow: '0 0 0 0 var(--pulse-color)',
+          },
+          '50%': {
+            boxShadow: '0 0 0 8px var(--pulse-color)',
+          },
+        },
       },
     },
   },
